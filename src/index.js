a36b12dd81ca2991ed5f2c91bb3f498b894bef06
AMaslenkina <nastya2003380@gmail.com>
2021-03-14 16:05:53 +0200

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const config = bracketsConfig.map(i => i.join(''));
+    let string = str, tmp;
+
+    do {
+        tmp = string;
+        config.forEach(i => string = string.replace(i, ''));
+    } while (string.length < tmp.length)
+
+    return !string.length;
 }
